eea6cfbf00774f564f95c0850443fe20719b5c88
Klaas Troost <klaas.troost.kip@gmail.com>
2020-03-06 18:45:37 +0100

    Eerste opzet Xamarin Forms 4.5 Releases.

---
+++ XF4_5Features/XF4_5Features/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("RijksoverheidSansWebText-Regular.ttf")]
+[assembly: ExportFont("RijksoverheidSansWebText-Bold.ttf")]
+[assembly: ExportFont("RijksoverheidSansWebText-Italic.ttf")]
---
+++ XF4_5Features/XF4_5Features/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms;
+
+namespace XF4_5Features
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetPrefersHomeIndicatorAutoHidden(true);
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetPreferredStatusBarUpdateAnimation(UIStatusBarAnimation.None);
+            
+            InitializeComponent();
+
+            VisualStateManager.GoToState(this, "Red");
+        }
+
+        void Button1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Red");
+        }
+
+        void Button2_Clicked(System.Object sender, System.EventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Green");
+        }
+
+        void Button3_Clicked(System.Object sender, System.EventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Blue");
+        }
+    }
+}
